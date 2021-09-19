6a701fba90ead99d66d2ecb1acd74e5a0771bbf1
Karthik Chandra Amudha <kca.mobile.dev@gmail.com>
2021-09-19 16:56:04 -0500

    Initial Commit

---
+++ iOS-TMDb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-TMDb
+//
+//  Created by Karthik Chandra Amudha on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
